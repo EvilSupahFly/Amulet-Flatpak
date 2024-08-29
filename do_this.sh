6d1c19bd62cbf89ea23276029db5d6d87aad9012
EvilSupahFly <seann.giffin@gmail.com>
2024-08-29 17:05:47 -0400

    Testing Update

--- do_this.sh
+++ do_this.sh
@@ -17,7 +17,7 @@ WHITE="${BOLD}${BGND}\e[1;97m" #Bold/Hi-int White
 
 function doFlatpakPIP {
     # Generate everything we need to build Amulet in the Flatpak sandbox
-    ./flatpak-pip-generator --requirements-file=requirements.txt --yaml --output=amulet_map_editor
+    ./flatpak-pip-generator --requirements-file=requirements.txt --yaml --output=flatpak-pip-modules
 
     # Create the initial header for our "proper" manifest
 cat << EOL > "io.github.evilsupahfly.amulet-flatpak.yml"
@@ -26,7 +26,6 @@ id: io.github.evilsupahfly.amulet-flatpak
 name: Amulet Map Editor
 runtime: org.freedesktop.Platform
 runtime-version: '23.08'
-sdk: org.freedesktop.Sdk//23.08
 
 command: amulet_map_editor
 
@@ -37,32 +36,19 @@ finish-args:
   - --socket=wayland
   - --device=dri
   - --filesystem=home:create
-  - --talk-name=org.freedesktop.Notifications
   - --env=LIBGL_ALWAYS_SOFTWARE="0"
   - --env=OPENGL_VERSION=3.3
   - --env=OPENGL_LIB=/usr/lib/x86_64-linux-gnu/libGL.so
-  - --env=PYTHON_VERSION=3.11.9
-  - --env=WX_PYTHON=/app/lib/python3.11/site-packages/wx
-  - --env=WX_PYTHON_VERSION=4.1.1
-  - --env=XAPP_GTK3=true
 
 modules:
   - shared-modules/SDL/SDL-1.2.15.json
   - shared-modules/SDL2/SDL2-with-libdecor.json
   - shared-modules/glew/glew.json
-  - shared-modules/glu/glu-9.json
-  - shared-modules/libappindicator/libappindicator-gtk3-introspection-12.10.json
-  - shared-modules/gtk2/gtk2.json
-  - shared-modules/dbus-glib/dbus-glib.json
-  - shared-modules/pygame/pygame-1.9.6.json
+  - flatpak-pip-modules.yaml
+  - name: flatpak-pip-modules
 
 #### <<< do_this.sh
 EOL
-
-    # Add the output from flatpak-pip-generator after cleaning up the temp file
-    sed -i "s/modules://g" "amulet_map_editor.yaml"
-    sed -i "s/name: amulet_map_editor//g" "amulet_map_editor.yaml"
-    cat "amulet_map_editor.yaml" >> "io.github.evilsupahfly.amulet-flatpak.yml"
 }
 
 if [[ "$1" == "do-pip" || "$1" == "-do-pip" || "$1" == "--do-pip" || "$1" == "-d" ]]; then
@@ -79,7 +65,7 @@ elif [[ "$1" == "help" || "$1" == "--help" ]]; then
     echo -e "\n${GREEN}\nRunning without ${WHITE}--do-pip${GREEN} will skip running"
     echo -e "${WHITE}flatpak-pip-generator${GREEN} to generate a new \"amulet.yml\"."
     echo -e "\nHowever, there's no error checking, so if ${WHITE}amulet.yml${GREEN} doesn't"
-    echo -e "exist, ${RED}this WILL all breakdown. ${GREEN}Buyer beware, right?${RESET}\n"
+    echo -e "exist, ${RED}this WILL all breakdown. ${GREEN}Buyer beware, and all that jazz.${RESET}\n"
     exit 0
 else
     echo -e "\n${YELLOW}    Skipping flatpak-pip-generator.${RESET}\n"
@@ -87,19 +73,19 @@ else
 fi
 
 # Attempt to build Frankenstein's Monster - change "tag" when updating to newer Amulet versions
-flatpak-builder -v --install-deps-from=flathub --mirror-screenshots-url=https://dl.flathub.org/media/ --add-tag=0.10.35 --bundle-sources --repo=io.github.evilsupahfly.amulet-flatpak amulet_build_dir amulet.yml --force-clean
+flatpak-builder -v --install-deps-from=flathub --mirror-screenshots-url=https://dl.flathub.org/media/ --add-tag=0.10.35 --bundle-sources --repo=io.github.evilsupahfly.amulet-flatpak-repo amulet-flatpak_build_dir io.github.evilsupahfly.amulet-flatpak.yml --force-clean
 
 # Bundle the contents of the local repository into "amulet.flatpak"
 flatpak build-bundle io.github.evilsupahfly.amulet-flatpak io.github.evilsupahfly.amulet-flatpak.flatpak io.github.evilsupahfly.amulet-flatpak
 
 # Install bundle
 echo -e "\n${YELLOW}    To install the Amulet Flatpak, type:"
-echo -e "${WHITE}        flatpak install -u amulet.flatpak\n"
+echo -e "${WHITE}        flatpak install -u amulet.flatpak"
 
 # Run bundle
 echo -e "\n${YELLOW}    To run your install, type:"
-echo -e "${WHITE}        flatpak run io.github.evilsupahfly.amulet-flatpak\n"
+echo -e "${WHITE}        flatpak run io.github.evilsupahfly.amulet-flatpak"
 
 #Uninstall bundle if it doesn't work or you just don't need it
 echo -e "\n${YELLOW}    To uninstall this, type:"
-echo -e "${RED}        flatpak uninstall io.github.evilsupahfly.amulet-flatpak${RESET}\n"
+echo -e "${RED}        flatpak uninstall io.github.evilsupahfly.amulet-flatpak${RESET} \n"
