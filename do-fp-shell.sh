440e6e9015d74aee3f560f67bfeb23b09ae5fec9
EvilSupahFly <seann.giffin@gmail.com>
2025-04-23 07:41:08 -0400

    Removed extra testing scripts

--- do-fp-shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-    cmd="sh"
-else
-    cmd="$1"
-fi
-
-flatpak run --ostree-verbose -vv --command="$cmd" --devel --filesystem="$(pwd)" io.github.evilsupahfly.amulet_flatpak
